775e95d047c79267773f82f01b8f5ba77124d871
Oleks Maistrenko <oleksandr.maistrenko@yandex.com>
2018-03-05 23:34:44 +0200

    added comments

--- audio.py
+++ audio.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+#
+# fetching music from youtube
+#
 import pafy
 import subprocess
 
@@ -8,12 +10,14 @@ player = None
 
 
 def download_and_play_youtube(video_id):
+    """ download the audio for the given youtube video id """
     global player
     youtube_video = pafy.new(video_id)
-    filename = youtube_video.getbestaudio().download(quiet=True)
+    filename = youtube_video.getbestaudio().download(quiet=True, meta=True)
     player = subprocess.Popen(['mplayer', '-ao', 'pulse::bluez_sink.A0_2C_36_77_25_96.a2dp_sink', '-volume', '50', filename],
                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
 def stop_player():
+    """ terminate the player """
     player.terminate()
